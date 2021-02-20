2ad748de111772f46d0c76a134a60c0776757813
Christopher Tse <chris@chris-tse.com>
2021-02-20 02:47:26 -0600

    Get skeleton function up

---
+++ netlify/functions/status.js
@@ -0,0 +1,13 @@
+exports.handler = async function (event, context) {
+    const NetcatClient = require('netcat')
+    const nc = new NetcatClient()
+    const SERVER = 'chrisminecraft.duckdns.org'
+    const PORT_RANGE = '255565-25565'
+
+    nc.addr(SERVER).scan(PORT_RANGE, ports => {
+        return {
+            statusCode: 200,
+            body: JSON.stringify(ports),
+        }
+    })
+}
