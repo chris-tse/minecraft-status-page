fbd1682d3112e49da7a847fa9387d688ce9054ac
Christopher Tse <chris@chris-tse.com>
2021-02-20 02:54:10 -0600

    Bad port range

--- netlify/functions/status.js
+++ netlify/functions/status.js
@@ -2,7 +2,7 @@ exports.handler = async function (event, context) {
     const NetcatClient = require('netcat/client')
     const nc = new NetcatClient()
     const SERVER = 'chrisminecraft.duckdns.org'
-    const PORT_RANGE = '255565-25565'
+    const PORT_RANGE = '25565-25565'
 
     nc.addr(SERVER).scan(PORT_RANGE, ports => {
         return {
