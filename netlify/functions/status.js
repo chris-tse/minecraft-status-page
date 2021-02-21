a4cd797ef26e5451da9a264316c80b66bbf704fd
Christopher Tse <chris@chris-tse.com>
2021-02-20 23:57:32 -0600

    Add headers for cors

--- netlify/functions/status.js
+++ netlify/functions/status.js
@@ -1,17 +1,24 @@
 exports.handler = async function (event, context) {
     const util = require('minecraft-server-util')
     const SERVER = 'chrisminecraft.duckdns.org'
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+    }
 
     try {
         const response = await util.status(SERVER)
 
         return {
             statusCode: 200,
+            headers,
             body: JSON.stringify({ error: false, serverInfo: response }),
         }
     } catch (e) {
         return {
             statusCode: 200,
+            headers,
             body: JSON.stringify({ error: true, message: e.message }),
         }
     }
