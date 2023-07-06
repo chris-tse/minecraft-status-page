225a33d8681a0f7496aac9770140d775d20800c0
Christopher Tse <chris@chris-tse.com>
2023-07-06 17:39:22 -0500

    update ddns url

--- netlify/functions/status.js
+++ netlify/functions/status.js
@@ -1,26 +1,26 @@
-const util = require('minecraft-server-util')
+const util = require('minecraft-server-util');
 
 exports.handler = async function (event, context) {
-    const SERVER = 'chrisminecraft.duckdns.org'
+    const SERVER = 'culturalserver.duckdns.org';
     const headers = {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Content-Type',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-    }
+    };
 
     try {
-        const response = await util.status(SERVER)
+        const response = await util.status(SERVER);
 
         return {
             statusCode: 200,
             headers,
             body: JSON.stringify({ error: false, serverInfo: response }),
-        }
+        };
     } catch (e) {
         return {
             statusCode: 200,
             headers,
             body: JSON.stringify({ error: true, message: e.message }),
-        }
+        };
     }
-}
+};
